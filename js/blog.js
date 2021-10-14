61fce3c8ba152a6ad417879af94d6eb988766f39
code-malloc <44perfectworld44@gmail.com>
2021-10-14 17:21:11 +0900

    commit

--- js/blog.js
+++ js/blog.js
@@ -1,1 +1,97 @@
-document.getElementById("link-article-1").onclick=function(){window.open("content/blog/1-kak-obnovit'-ubuntu-cherez-terminal.html","_self")};document.getElementById("link-article-2").onclick=function(){window.open("content/blog/2-kak-otpravit'-sushchestvuyushchiy-proekt-na-github.html","_self")};document.getElementById("link-article-3").onclick=function(){window.open("content/blog/3-linux-rabota-s-katalogami-i-faylami-chast'-pervaya.html","_self")};document.getElementById("link-article-4").onclick=function(){window.open("content/blog/4-linux-rabota-s-katalogami-i-faylami-chast'-vtoraya.html","_self")};document.getElementById("link-article-5").onclick=function(){window.open("content/blog/5-hello-world-na-si.html","_self")};document.getElementById("link-article-6").onclick=function(){window.open("content/blog/6-converter-valyut-na-si.html","_self")};document.getElementById("link-article-7").onclick=function(){window.open("content/blog/7-otzhimaniya-v-otkaz-programma.html","_self")};document.getElementById("link-article-8").onclick=function(){window.open("content/blog/8-tri-zolotykh-uprazhneniya-programma.html","_self")};document.getElementById("link-article-9").onclick=function(){window.open("content/blog/9-raspredeleniye-maksimal'nogo-kolichestva-povtoreniy-na-podkhody.html","_self")};document.getElementById("link-article-10").onclick=function(){window.open("content/blog/10-yedinorog.html","_self")};document.getElementById("Donat-footer-d").onclick=function(){window.open("https://www.donationalerts.com/c/malloc","_blank")};document.getElementById("Telegram-footer-d").onclick=function(){window.open("https://msngr.link/tg/malloc_ru","_blank")};document.getElementById("Youtube-footer-d").onclick=function(){window.open("https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw","_blank")};document.getElementById("GitHub-footer-d").onclick=function(){window.open("https://github.com/code-malloc","_blank")};document.getElementById("Topmail-footer-d").onclick=function(){window.open("https://top.mail.ru/jump?from=3211268","_blank")};document.getElementById("Payeer-footer-d").onclick=function(){window.open("https://payeer.com/04184305","_blank")};document.getElementById("Donat-footer-m").onclick=function(){window.open("https://www.donationalerts.com/c/malloc","_blank")};document.getElementById("Telegram-footer-m").onclick=function(){window.open("https://msngr.link/tg/malloc_ru","_blank")};document.getElementById("Youtube-footer-m").onclick=function(){window.open("https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw","_blank")};document.getElementById("GitHub-footer-m").onclick=function(){window.open("https://github.com/code-malloc","_blank")};document.getElementById("Topmail-footer-m").onclick=function(){window.open("https://top.mail.ru/jump?from=3211268","_blank")};document.getElementById("Payeer-footer-m").onclick=function(){window.open("https://payeer.com/04184305","_blank")};var mySidebar=document.getElementById("mySidebar");var overlayBg=document.getElementById("myOverlay");function malloc_open(){if(mySidebar.style.display==="block"){mySidebar.style.display="none";overlayBg.style.display="none"}else{mySidebar.style.display="block";overlayBg.style.display="block"}}function malloc_close(){mySidebar.style.display="none";overlayBg.style.display="none"}function date(){var c=new Date();var a=new Array("Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота");var b=new Array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");return" "+a[c.getDay()]+" "+c.getDate()+" "+b[c.getMonth()]+" "+c.getFullYear()+" "}setInterval(function(){document.getElementById("mydate").innerHTML=date()},0);function zero_first_format(a){if(a<10){a="0"+a}return a}function time(){var d=new Date();var a=zero_first_format(d.getHours());var b=zero_first_format(d.getMinutes());var c=zero_first_format(d.getSeconds());return" "+a+":"+b+":"+c+" "}setInterval(function(){document.getElementById("mytime").innerHTML=time()});var _tmr=window._tmr||(window._tmr=[]);_tmr.push({id:"3211268",type:"pageView",start:(new Date()).getTime()});(function(e,a,g){if(e.getElementById(g)){return}var b=e.createElement("script");b.type="text/javascript";b.async=true;b.id=g;b.src="https://top-fwz1.mail.ru/js/code.js";var c=function(){var d=e.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d)};if(a.opera=="[object Opera]"){e.addEventListener("DOMContentLoaded",c,false)}else{c()}})(document,window,"topmailru-code");var acc=document.getElementsByClassName("malloc-accordion");var i;for(i=0;i<acc.length;i++){acc[i].addEventListener("click",function(){this.classList.toggle("active");var a=this.nextElementSibling;if(a.style.display==="block"){a.style.display="none"}else{a.style.display="block"}})};
+/* Статьи */
+document.getElementById('link-article-1').onclick=function(){window.open('content/blog/1-kak-obnovit\'-ubuntu-cherez-terminal.html', '_self');};
+document.getElementById('link-article-2').onclick=function(){window.open('content/blog/2-kak-otpravit\'-sushchestvuyushchiy-proekt-na-github.html', '_self');};
+document.getElementById('link-article-3').onclick=function(){window.open('content/blog/3-linux-rabota-s-katalogami-i-faylami-chast\'-pervaya.html', '_self');};
+document.getElementById('link-article-4').onclick=function(){window.open('content/blog/4-linux-rabota-s-katalogami-i-faylami-chast\'-vtoraya.html', '_self');};
+document.getElementById('link-article-5').onclick=function(){window.open('content/blog/5-hello-world-na-si.html', '_self');};
+document.getElementById('link-article-6').onclick=function(){window.open('content/blog/6-converter-valyut-na-si.html', '_self');};
+document.getElementById('link-article-7').onclick=function(){window.open('content/blog/7-otzhimaniya-v-otkaz-programma.html', '_self');};
+document.getElementById('link-article-8').onclick=function(){window.open('content/blog/8-tri-zolotykh-uprazhneniya-programma.html', '_self');};
+document.getElementById('link-article-9').onclick=function(){window.open('content/blog/9-raspredeleniye-maksimal\'nogo-kolichestva-povtoreniy-na-podkhody.html', '_self');};
+document.getElementById('link-article-10').onclick=function(){window.open('content/blog/10-yedinorog.html', '_self');};
+
+/* Footer desktop*/
+document.getElementById('Donat-footer-d').onclick=function(){window.open('https://www.donationalerts.com/c/malloc', '_blank');};
+document.getElementById('Telegram-footer-d').onclick=function(){window.open('https://msngr.link/tg/malloc_ru', '_blank');};
+document.getElementById('Youtube-footer-d').onclick=function(){window.open('https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw', '_blank');};
+document.getElementById('GitHub-footer-d').onclick=function(){window.open('https://github.com/code-malloc', '_blank');};
+document.getElementById('Topmail-footer-d').onclick=function(){window.open('https://top.mail.ru/jump?from=3211268', '_blank');};
+document.getElementById('Payeer-footer-d').onclick=function(){window.open('https://payeer.com/04184305', '_blank');};
+
+/* Footer mobile*/
+document.getElementById('Donat-footer-m').onclick=function(){window.open('https://www.donationalerts.com/c/malloc', '_blank');};
+document.getElementById('Telegram-footer-m').onclick=function(){window.open('https://msngr.link/tg/malloc_ru', '_blank');};
+document.getElementById('Youtube-footer-m').onclick=function(){window.open('https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw', '_blank');};
+document.getElementById('GitHub-footer-m').onclick=function(){window.open('https://github.com/code-malloc', '_blank');};
+document.getElementById('Topmail-footer-m').onclick=function(){window.open('https://top.mail.ru/jump?from=3211268', '_blank');};
+document.getElementById('Payeer-footer-m').onclick=function(){window.open('https://payeer.com/04184305', '_blank');};
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* sidebar */
+var mySidebar = document.getElementById("mySidebar");
+var overlayBg = document.getElementById("myOverlay");
+function malloc_open() {if (mySidebar.style.display === 'block') {mySidebar.style.display = 'none';overlayBg.style.display = "none";} else {mySidebar.style.display = 'block';overlayBg.style.display = "block";}}
+function malloc_close() {mySidebar.style.display = "none";overlayBg.style.display = "none";}
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* date */
+function date()
+{
+	var d = new Date();
+	var day=new Array("Воскресенье","Понедельник","Вторник", "Среда","Четверг","Пятница","Суббота");
+	var month=new Array("января","февраля","марта","апреля","мая","июня", "июля","августа","сентября","октября","ноября","декабря");
+	return " " +day[d.getDay()]+" " +d.getDate()+ " " + month[d.getMonth()] + " " + d.getFullYear() + " ";
+}
+setInterval(function () {document.getElementById('mydate').innerHTML = date();}, 0);
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* time */		
+function zero_first_format(value)		
+{
+	if (value < 10)
+	{
+		value='0'+value;
+	}
+	return value;
+}
+function time()
+{
+	var current_time = new Date();
+	var hours = zero_first_format(current_time.getHours());
+	var minutes = zero_first_format(current_time.getMinutes());
+	var seconds = zero_first_format(current_time.getSeconds());
+	return " "+hours+":"+minutes+":"+seconds+" ";
+};
+setInterval(function () {document.getElementById('mytime').innerHTML = time();});
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* Rating Mail.ru counter */
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3211268", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "topmailru-code");
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* JS Гармошка */
+var acc = document.getElementsByClassName("malloc-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Переключение между добавлением и удалением "active" class, выделить кнопку, управляющую панелью */
+    this.classList.toggle("active");
+
+    /* Переключение между скрытием и отображением активной панели */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,87 @@
-document.getElementById("malloc.ru").onclick=function(){window.open("https://malloc.ru","_self")};document.getElementById("Blog").onclick=function(){window.open("blog.html","_self")};document.getElementById("Telegram").onclick=function(){window.open("https://msngr.link/tg/malloc_ru","_blank")};document.getElementById("Youtube").onclick=function(){window.open("https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw","_blank")};document.getElementById("GitHub").onclick=function(){window.open("https://github.com/code-malloc","_blank")};document.getElementById("Donat").onclick=function(){window.open("donat.html","_self")};document.getElementById("Donat-footer-d").onclick=function(){window.open("https://www.donationalerts.com/c/malloc","_blank")};document.getElementById("Telegram-footer-d").onclick=function(){window.open("https://msngr.link/tg/malloc_ru","_blank")};document.getElementById("Youtube-footer-d").onclick=function(){window.open("https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw","_blank")};document.getElementById("GitHub-footer-d").onclick=function(){window.open("https://github.com/code-malloc","_blank")};document.getElementById("Topmail-footer-d").onclick=function(){window.open("https://top.mail.ru/jump?from=3211268","_blank")};document.getElementById("Payeer-footer-d").onclick=function(){window.open("https://payeer.com/04184305","_blank")};document.getElementById("Donat-footer-m").onclick=function(){window.open("https://www.donationalerts.com/c/malloc","_blank")};document.getElementById("Telegram-footer-m").onclick=function(){window.open("https://msngr.link/tg/malloc_ru","_blank")};document.getElementById("Youtube-footer-m").onclick=function(){window.open("https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw","_blank")};document.getElementById("GitHub-footer-m").onclick=function(){window.open("https://github.com/code-malloc","_blank")};document.getElementById("Topmail-footer-m").onclick=function(){window.open("https://top.mail.ru/jump?from=3211268","_blank")};document.getElementById("Payeer-footer-m").onclick=function(){window.open("https://payeer.com/04184305","_blank")};var mySidebar=document.getElementById("mySidebar");var overlayBg=document.getElementById("myOverlay");function malloc_open(){if(mySidebar.style.display==="block"){mySidebar.style.display="none";overlayBg.style.display="none"}else{mySidebar.style.display="block";overlayBg.style.display="block"}}function malloc_close(){mySidebar.style.display="none";overlayBg.style.display="none"}function date(){var c=new Date();var a=new Array("Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота");var b=new Array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");return" "+a[c.getDay()]+" "+c.getDate()+" "+b[c.getMonth()]+" "+c.getFullYear()+" "}setInterval(function(){document.getElementById("mydate").innerHTML=date()},0);function zero_first_format(a){if(a<10){a="0"+a}return a}function time(){var d=new Date();var a=zero_first_format(d.getHours());var b=zero_first_format(d.getMinutes());var c=zero_first_format(d.getSeconds());return" "+a+":"+b+":"+c+" "}setInterval(function(){document.getElementById("mytime").innerHTML=time()});var _tmr=window._tmr||(window._tmr=[]);_tmr.push({id:"3211268",type:"pageView",start:(new Date()).getTime()});(function(e,a,g){if(e.getElementById(g)){return}var b=e.createElement("script");b.type="text/javascript";b.async=true;b.id=g;b.src="https://top-fwz1.mail.ru/js/code.js";var c=function(){var d=e.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d)};if(a.opera=="[object Opera]"){e.addEventListener("DOMContentLoaded",c,false)}else{c()}})(document,window,"topmailru-code");
+/* Ссылка malloc.ru */
+document.getElementById('malloc.ru').onclick=function(){window.open('https://malloc.ru', '_self');};
+
+/* Статьи */
+document.getElementById('Blog').onclick=function(){window.open('blog.html', '_self');};
+
+/* Cоцсети */
+document.getElementById('Telegram').onclick=function(){window.open('https://msngr.link/tg/malloc_ru', '_blank');};
+document.getElementById('Youtube').onclick=function(){window.open('https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw', '_blank');};
+document.getElementById('GitHub').onclick=function(){window.open('https://github.com/code-malloc', '_blank');};
+
+/* Донат */
+document.getElementById('Donat').onclick=function(){window.open('donat.html', '_self');};
+
+/* Footer desktop*/
+document.getElementById('Donat-footer-d').onclick=function(){window.open('https://www.donationalerts.com/c/malloc', '_blank');};
+document.getElementById('Telegram-footer-d').onclick=function(){window.open('https://msngr.link/tg/malloc_ru', '_blank');};
+document.getElementById('Youtube-footer-d').onclick=function(){window.open('https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw', '_blank');};
+document.getElementById('GitHub-footer-d').onclick=function(){window.open('https://github.com/code-malloc', '_blank');};
+document.getElementById('Topmail-footer-d').onclick=function(){window.open('https://top.mail.ru/jump?from=3211268', '_blank');};
+document.getElementById('Payeer-footer-d').onclick=function(){window.open('https://payeer.com/04184305', '_blank');};
+
+/* Footer mobile*/
+document.getElementById('Donat-footer-m').onclick=function(){window.open('https://www.donationalerts.com/c/malloc', '_blank');};
+document.getElementById('Telegram-footer-m').onclick=function(){window.open('https://msngr.link/tg/malloc_ru', '_blank');};
+document.getElementById('Youtube-footer-m').onclick=function(){window.open('https://www.youtube.com/channel/UCIAboMgPOBcFR9Y5GoUdoTw', '_blank');};
+document.getElementById('GitHub-footer-m').onclick=function(){window.open('https://github.com/code-malloc', '_blank');};
+document.getElementById('Topmail-footer-m').onclick=function(){window.open('https://top.mail.ru/jump?from=3211268', '_blank');};
+document.getElementById('Payeer-footer-m').onclick=function(){window.open('https://payeer.com/04184305', '_blank');};
+/* null */
+/* 	document.getElementById('#null').onclick=function(){window.open('null', '_blank');};
+*/
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* sidebar */
+var mySidebar = document.getElementById("mySidebar");
+var overlayBg = document.getElementById("myOverlay");
+function malloc_open() {if (mySidebar.style.display === 'block') {mySidebar.style.display = 'none';overlayBg.style.display = "none";} else {mySidebar.style.display = 'block';overlayBg.style.display = "block";}}
+function malloc_close() {mySidebar.style.display = "none";overlayBg.style.display = "none";}
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* date */
+function date()
+{
+	var d = new Date();
+	var day=new Array("Воскресенье","Понедельник","Вторник", "Среда","Четверг","Пятница","Суббота");
+	var month=new Array("января","февраля","марта","апреля","мая","июня", "июля","августа","сентября","октября","ноября","декабря");
+	return " " +day[d.getDay()]+" " +d.getDate()+ " " + month[d.getMonth()] + " " + d.getFullYear() + " ";
+}
+setInterval(function () {document.getElementById('mydate').innerHTML = date();}, 0);
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* time */		
+function zero_first_format(value)		
+{
+	if (value < 10)
+	{
+		value='0'+value;
+	}
+	return value;
+}
+function time()
+{
+	var current_time = new Date();
+	var hours = zero_first_format(current_time.getHours());
+	var minutes = zero_first_format(current_time.getMinutes());
+	var seconds = zero_first_format(current_time.getSeconds());
+	return " "+hours+":"+minutes+":"+seconds+" ";
+};
+setInterval(function () {document.getElementById('mytime').innerHTML = time();});
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* Rating Mail.ru counter */
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3211268", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "topmailru-code");
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/* Редирект - перенаправление на страницу malloc.ru/index.html */
+//	location.replace("https://malloc.ru");
+//	document.location.replace("https://malloc.ru");
